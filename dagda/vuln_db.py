ebc5c2cb5a000fde08ee02f994c4e71c2723728c
eliasgranderubio <eliasgr89@gmail.com>
2016-11-26 20:43:36 +0100

    Minor refactor

--- dagda/vuln_db.py
+++ dagda/vuln_db.py
@@ -10,23 +10,22 @@ def main(parsed_args):
         # Init db
         db_composer = DBComposer()
         db_composer.compose_vuln_db()
-    elif parsed_args.get_cve():
-        # Get product from CVE
-        m = MongoDbDriver()
-        print(json.dumps(m.get_products_from_cve(parsed_args.get_cve()), sort_keys=True, indent=4))
-    elif parsed_args.get_bid():
-        # Get product from BID
-        m = MongoDbDriver()
-        print(json.dumps(m.get_products_from_bid(parsed_args.get_bid()), sort_keys=True, indent=4))
-    elif parsed_args.get_exploit_db_id():
-        # Get product from Exploit DB Id
-        m = MongoDbDriver()
-        print(json.dumps(m.get_products_from_exploit_db_id(parsed_args.get_exploit_db_id()), sort_keys=True, indent=4))
     else:
         m = MongoDbDriver()
-        # Gets CVEs, BIDs and Exploit_DB Ids
-        print(json.dumps(m.get_vulnerabilities(parsed_args.get_product(), parsed_args.get_product_version()),
-                         sort_keys=True, indent=4))
+        if parsed_args.get_cve():
+            # Get product from CVE
+            print(json.dumps(m.get_products_from_cve(parsed_args.get_cve()), sort_keys=True, indent=4))
+        elif parsed_args.get_bid():
+            # Get product from BID
+            print(json.dumps(m.get_products_from_bid(parsed_args.get_bid()), sort_keys=True, indent=4))
+        elif parsed_args.get_exploit_db_id():
+            # Get product from Exploit DB Id
+            print(json.dumps(m.get_products_from_exploit_db_id(parsed_args.get_exploit_db_id()),
+                             sort_keys=True, indent=4))
+        else:
+            # Gets CVEs, BIDs and Exploit_DB Ids
+            print(json.dumps(m.get_vulnerabilities(parsed_args.get_product(), parsed_args.get_product_version()),
+                             sort_keys=True, indent=4))
 
 
 if __name__ == "__main__":

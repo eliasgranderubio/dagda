3d67ec22a4aed92021a076d810b3228fcf43a98d
Elias Grande <eliasgr89@gmail.com>
2018-04-18 11:58:23 +0200

    Added some UT

--- tests/api/service/test_check.py
+++ tests/api/service/test_check.py
@@ -40,7 +40,7 @@ class CheckApiTestCase(unittest.TestCase):
         def get_docker_image_name_by_container_id(self, id):
             return 'redis'
 
-        def docker_pull(self, image_name):
+        def docker_pull(self, image_name, tag=None):
             return ''
 
         def is_docker_image(self, image_name):
@@ -94,6 +94,13 @@ class CheckApiTestCase(unittest.TestCase):
     @patch('api.internal.internal_server.InternalServer.get_dagda_edn', return_value=MockDagdaEdn())
     def test_check_docker_by_image_name_202_and_pull(self, m1, m2, m3):
         response, code = check_docker_by_image_name('fake_id')
+        self.assertEqual(code, 202)
+
+    @patch('api.internal.internal_server.InternalServer.get_docker_driver', return_value=MockDockerDriverPull())
+    @patch('api.internal.internal_server.InternalServer.get_mongodb_driver', return_value=MockMongoDriver())
+    @patch('api.internal.internal_server.InternalServer.get_dagda_edn', return_value=MockDagdaEdn())
+    def test_check_docker_by_image_name_and_tag_202_and_pull(self, m1, m2, m3):
+        response, code = check_docker_by_image_name('fake_id:latest')
         self.assertEqual(code, 202)
 
     @patch('api.internal.internal_server.InternalServer.get_docker_driver', return_value=MockDockerDriverPullError())

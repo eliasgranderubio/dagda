5e960e13840ab33833e9549cc99b880395c87516
Wesley Griffing <wgriffing@cyberpointllc.com>
2021-09-11 11:40:22 -0400

    Make helm-specific run.sh

--- dockerfiles/run.sh
+++ dockerfiles/run.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-freshclam
-service clamav-daemon restart
-service docker start
-sleep 10
-python dagda.py start -s 0.0.0.0 -p 5004 -m vulndb -mp 27017 --debug
+python dagda.py start -s 0.0.0.0 -p 5000 -m vulndb -mp 27017 --debug
